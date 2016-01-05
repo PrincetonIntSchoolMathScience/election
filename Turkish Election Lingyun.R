8227c4e39fa2305cbc794ecd23f8d876c402c3a6
Lingyun Xiao <bowlxly@gmail.com>
2016-01-05 13:17:07 -0500

    Turkish Election Fraud 2nd Commit

--- Turkish Election Lingyun.R
+++ Turkish Election Lingyun.R
@@ -7,9 +7,13 @@ library(dplyr)
 df <- select(ankara, 9, 12, 15, 21, 29, 31)
 
 library(reshape2)
-df_l <- melt(df, id=c("oy_kullanan_kayitli_secmen", "itirazsiz_gecerli_oy", "gecersiz_oy"))
+df_l <- melt(df, id=c("itirazsiz_gecerli_oy", "gecersiz_oy"))
+head(df_l)
 
+df_n <- group_by(df_l, variable) %>% 
+        summarize(s = sum(value))
 
-df_n <- group_by(df_l, variable) %>% summarize(s = sum(value))
-barplot(df_n$s, by=variable)
+library(ggplot2)
+ggplot(df_n, aes(x=variable, y=s)) + geom_bar(stat="sum")
+
 
