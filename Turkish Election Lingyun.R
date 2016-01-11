620cf2e3f4038fe843b0fbf1e8b6f4ca91325ea7
Lingyun Xiao <bowlxly@gmail.com>
2016-01-11 10:35:54 -0500

    Added explanation and new column of the turn out rate

--- Turkish Election Lingyun.R
+++ Turkish Election Lingyun.R
@@ -1,8 +1,10 @@
+#Read and summarize the data 
 ankara <- read.csv("ankara.csv")
 head(ankara)
 summary(ankara)
 names(ankara)
 
+#Select and reshape the dataset
 library(dplyr)
 df <- select(ankara, 9, 12, 15, 21, 29, 31)
 
@@ -13,6 +15,7 @@ head(df_l)
 df_n <- group_by(df_l, variable) %>% 
         summarize(s = sum(value))
 
+#Make the bar plot that represents the votes from major parties
 library(ggplot2)
 ggplot(df_n, aes(x=variable, y=s)) + 
   geom_bar(stat="sum") +
@@ -20,5 +23,8 @@ ggplot(df_n, aes(x=variable, y=s)) +
   ylab("votes") +
   ggtitle("Turkish election vote summary")
 
+#Subtract akp and chp number of votes
 ankara$akp_chp=(ankara$akp_oy-ankara$chp_oy)
-ankara$akp_chp
+
+ankara$turn_out_rates=(ankara$itirazsiz_gecerli_oy/ankara$oy_kullanan_kayitli_secmen)
+ankara$turn_out_rates
