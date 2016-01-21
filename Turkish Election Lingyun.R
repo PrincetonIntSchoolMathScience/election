f5652d501ce1deca93c42dd3e4e401ede4dfe723
Lingyun Xiao <bowlxly@gmail.com>
2016-01-21 11:24:13 -0500

    Changed plot style and calculation of turnout rates

--- Turkish Election Lingyun.R
+++ Turkish Election Lingyun.R
@@ -17,14 +17,31 @@ df_n <- group_by(df_l, variable) %>%
 
 #Make the bar plot that represents the votes from major parties
 library(ggplot2)
-ggplot(df_n, aes(x=variable, y=s)) + 
+pl <- ggplot(df_n, aes(x=variable, y=s)) + 
   geom_bar(stat="sum") +
   xlab("party name") +
   ylab("votes") +
-  ggtitle("Turkish election vote summary")
+  ggtitle("Turkish election vote summary") +
+  scale_x_discrete(breaks=c("oy_kullanan_kayitli_secmen", "akp_oy", "chp_oy", "mhp_oy"),
+                   labels=c("Total Votes", "Justice and Development Party", "Republican People's Party", "Nationalist Movement Party"))
+pl + theme(axis.text.x = element_text(angle=30, hjust=1, vjust=1)) + theme(legend.position="none")
 
-#Subtract akp and chp number of votes
-ankara$akp_chp=(ankara$akp_oy-ankara$chp_oy)
+#Add akp and chp number of votes
+ankara$akp_plus_chp=(ankara$akp_oy+ankara$chp_oy)
 
-ankara$turn_out_rates=(ankara$itirazsiz_gecerli_oy/ankara$oy_kullanan_kayitli_secmen)
-ankara$turn_out_rates
+#Calculate the AKP vote share
+ankara$akp_vote_share=(ankara$akp_oy/ankara$akp_plus_chp)
+
+library(dplyr)
+
+#Divide valid votes and registered votes gives the turnout rate
+ankara <- mutate(ankara, turn_out_rates = gecerli_oy/kayitli_secmen)
+ankara <- mutate(ankara, turn_out_rates2 = kullanilan_toplam_oy/kayitli_secmen)
+
+#Make a scatter plot regarding akp-chp and turnout rates
+pl2 <- ggplot(ankara, aes(x=turn_out_rates, y=akp_vote_share),
+              ggtitle="AKP Vote Share vs turnout")
+pl2 + geom_point(size=2, colour="blue") +xlim(0.2, 1.2) +ylim(0, 1) +
+                 xlab("Turnout") +
+                 ylab("AKP Vote Share") +
+  stat_smooth(color="red")
